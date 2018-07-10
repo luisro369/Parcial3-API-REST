cb7ba6db577ba8239974b79de565e6a39f6bc7a9
Sortweste <37189761+Sortweste@users.noreply.github.com>
2018-07-10 08:32:20 -0600

    Mongo on Index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 'use strict'
 const app = require('./app')
 const config = require('./config')
+const mongoose = require('mongoose')
+
 
 app.listen(config.port, () => {
 		console.log(`http://localhost:${config.port}`)
