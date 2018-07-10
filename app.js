5760cedba53dbef618eb666099bf73e388171dd6
Sortweste <37189761+Sortweste@users.noreply.github.com>
2018-07-10 08:03:49 -0600

    Models Structure

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view-engine','pug')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-//app.use('/api', api)
+//	/app.use('/api', api)
 
 app.get('/', function(req, res){
 	res.render('loginWeb.pug', {title: "Login"})
