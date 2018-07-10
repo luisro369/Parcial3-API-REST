2407d73580084d821d06e6980e34944dc87cc554
Sortweste <37189761+Sortweste@users.noreply.github.com>
2018-07-10 08:36:55 -0600

    Encrypt libraries

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,11 @@
 
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const bcrypt = require('bcrypt-nodejs')
+
 const UserSchema = new Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	username: {type: String, required: true, default: "John Doe"},
@@ -6,3 +13,14 @@ const UserSchema = new Schema({
 	email: {type: String, unique: true, lowercase: true},
 	phone: {type: String, required: true},
 })
+
+UserSchema.methods.generateHash = function(password){
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(9))
+}
+
+UserSchema.methods.validPassword = function(password){
+	return bcrypt.compareSync(password, this.password)
+}
+
+
+module.exports = mongoose.model('User', UserSchema)
