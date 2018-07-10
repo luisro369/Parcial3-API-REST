50d6f47c48fe6672e11e00f02948ccaa4e3b284b
Karla <00022516@uca.edu.sv>
2018-07-10 09:22:15 -0600

    arreglando errores

--- models/cardModel.js
+++ models/cardModel.js
@@ -1,4 +1,6 @@
 //paquete de mongoose
+'use strict'
+
 const mongoose = require('mongoose');
 
 //creacion del Schema de productos
@@ -6,7 +8,7 @@ const cardSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   title: {type: String, required: true},
   image: {type: String, required: true},
-  description: {type: String, required: true}
+  description: {type: String, required: true},
   _id_album: {type: String, required: true},
   type: {type: String, required: true},
   quality: {type: String, required: true}
