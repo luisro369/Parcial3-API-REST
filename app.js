3efd3768fb51500e4eebb5d553c103d965170274
Sortweste <37189761+Sortweste@users.noreply.github.com>
2018-07-10 07:03:04 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.set('view-engine','pug')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+//app.use('/api', api)
+
+app.get('/', function(req, res){
+	res.render('loginWeb.pug', {title: "Login"})
+})
+
+module.exports = app
